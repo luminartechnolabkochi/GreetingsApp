42f8c43886be08fd7914836f710349de083e80b9
sajay <sajaykannan10@gmail.com>
2024-08-23 15:12:38 +0530

    initila

---
+++ greetings/urls.py
@@ -0,0 +1,40 @@
+"""
+URL configuration for greetings project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from myapp.views import HelloWorldView,GoodMorningView,GoodAfternoonView,greetings_view,SelfIntroView,ActorView
+
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    path("helloworld/",HelloWorldView.as_view()),
+
+    path("morning/",GoodMorningView.as_view()),
+
+    path("afternoon/",GoodAfternoonView.as_view()),
+
+    path("greetings/",greetings_view),
+
+    path("intro/",SelfIntroView.as_view()),
+
+    path("actor/",ActorView.as_view())
+
+
+]
---
+++ myapp/views.py
@@ -0,0 +1,76 @@
+from django.shortcuts import render
+
+from django.views.generic import View
+
+
+# url:localhost:8000/helloworld
+# method:get
+# response=>helloworld.html
+
+
+class HelloWorldView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,"helloworld.html")
+
+
+class GoodMorningView(View):
+
+    def get(self,request,*args,**kwrags):
+
+        return render(request,"morning.html")
+
+
+class GoodAfternoonView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        return render(request,"afternoon.html")
+
+
+
+
+
+def greetings_view(request,*args,**kwargs):
+
+    return render(request,"greetings.html")
+
+
+
+class SelfIntroView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        data={
+            "name":"sarath Babu",
+            "age":21,
+            "qualification":"bsc",
+            "contact":4567890,
+            "course":"cs"
+        }
+
+        return render(request,"selfintro.html",data)
+
+
+# localhost:8000/mammooty/
+# method:get
+
+# 5Actors
+
+class ActorView(View):
+
+    def get(self,request,*args,**kwargs):
+
+        data={
+            "name":"Mammoty",
+            "profile_pic":"https://images.indianexpress.com/2024/05/mammootty-fb.jpg",
+            "age":"70",
+            "dob":"345678"
+        }
+
+        return render(request,"actor_detail.html",data)
+
+
+
+   
